712140d42e1c6eac0405d7989cbb2fb1a2018f4e
furquanh <64953180+furquanh@users.noreply.github.com>
2023-12-26 22:04:39 -0500

    Excercise 1

---
+++ React_introduction/Excercise/goals/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyList from './components/list' 
+import Header from './components/header'
+
+function App() {
+  return (
+      <>
+    <Header />
+    <MyList />
+      </>
+  );
+}
+
+export default App;
